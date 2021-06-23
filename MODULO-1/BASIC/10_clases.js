af1dcda056e165a1bedff45a7edfb9a7514591cb
Juan Pablo Ortiz <juanpablo.50cn@hotmail.com>
2021-06-23 15:21:07 -0500

    Clase Persona

--- MODULO-1/BASIC/10_clases.js
+++ MODULO-1/BASIC/10_clases.js
@@ -1,25 +1,29 @@
-// como se hacia antes 
+//COMO SE HARIA ANTES
+function Rectangulo(base,altura){
+    this.base=base
+    this.altura=altura
+}
 
-var rectangulo = new Rectangulo(6, 3)
+Rectangulo.prototype.calcularArea=function(){
+    return this.basethis.altura
+}
+var rectangulo=new Rectangulo(6,3)
+//console.log(rectangulo.calcularArea());
+//console.log(rectangulo.base);
 
-console.log(rectangulo.CalcularArea())
-console.log(rectangulo.base)
+//DECLARACION DE CLASES
 
-//  Declaracion de clases
 class Rectangulo2{
-    constructor(base, altura) {
-        this.base = base 
+    constructor(base, altura){
+        this.base = base
         this.altura = altura
     }
-
-    CalcularArea(){
+    calcularArea(){
         return this.base * this.altura
     }
 }
-
-var Rectangulo2 = new Rectangulo2 (6, 3)
-
-console.log(Rectangulo2.CalcularArea())
+var rectangulos2=new Rectangulo2(2,3)
+// console.log(rectangulos2.calcularArea())
 
 // Clase Persona
 // implementar getters, setter y realizar una implementacion.
@@ -32,3 +36,34 @@ console.log(Rectangulo2.CalcularArea())
 deber
 
 */
+
+
+class Persona {
+    constructor(){
+        this._nombre = null;
+        this._apellido = null;
+        this._edad = null;
+    }  
+    get nombre(){
+        return this._nombre
+    }get apellido(){
+        return this._apellido
+    }get edad(){
+        return this._edad
+    }
+    set nombre(nom) {
+        this._nombre = nom
+    }
+    set apellido(ape){
+        this._apellido = ape
+    }
+    set edad(age){
+        this._edad = age;
+    }
+}
+
+
+Persona.nombre ="Juan";
+Persona.apellido="Ortiz"
+Persona.edad="21"
+console.log("Hola mi nombre es " + Persona.nombre + " " +  Persona.apellido + " y mi edad es " + Persona.edad + " año(s)");

2f88f5b9c496652b6cfd1c0050a04bf89e4de906
Juan Pablo Ortiz <juanpablo.50cn@hotmail.com>
2021-06-23 16:51:25 -0500

    Deber

--- MODULO-1/BASIC/10_clases.js
+++ MODULO-1/BASIC/10_clases.js
@@ -1,28 +1,28 @@
 //COMO SE HARIA ANTES
-function Rectangulo(base,altura){
-    this.base=base
-    this.altura=altura
+function Rectangulo(base, altura) {
+  this.base = base;
+  this.altura = altura;
 }
 
-Rectangulo.prototype.calcularArea=function(){
-    return this.basethis.altura
-}
-var rectangulo=new Rectangulo(6,3)
+Rectangulo.prototype.calcularArea = function () {
+  return this.base + this.altura;
+};
+var rectangulo = new Rectangulo(6, 3);
 //console.log(rectangulo.calcularArea());
 //console.log(rectangulo.base);
 
 //DECLARACION DE CLASES
 
-class Rectangulo2{
-    constructor(base, altura){
-        this.base = base
-        this.altura = altura
-    }
-    calcularArea(){
-        return this.base * this.altura
-    }
+class Rectangulo2 {
+  constructor(base, altura) {
+    this.base = base;
+    this.altura = altura;
+  }
+  calcularArea() {
+    return this.base * this.altura;
+  }
 }
-var rectangulos2=new Rectangulo2(2,3)
+var rectangulos2 = new Rectangulo2(2, 3);
 // console.log(rectangulos2.calcularArea())
 
 // Clase Persona
@@ -30,40 +30,76 @@ var rectangulos2=new Rectangulo2(2,3)
 // implementar metodos estaticos - Clase Punto, Function distancia
 // Construir clase Persona y estudiante, extender Estudiante con Persona
 
-
 /*
  
 deber
 
 */
 
-
 class Persona {
-    constructor(){
-        this._nombre = null;
-        this._apellido = null;
-        this._edad = null;
-    }  
-    get nombre(){
-        return this._nombre
-    }get apellido(){
-        return this._apellido
-    }get edad(){
-        return this._edad
-    }
-    set nombre(nom) {
-        this._nombre = nom
-    }
-    set apellido(ape){
-        this._apellido = ape
-    }
-    set edad(age){
-        this._edad = age;
-    }
+  constructor(nombre, apellido, edad) {
+    this._nombre = nombre;
+    this._apellido = apellido;
+    this._edad = edad;
+  }
+  get nombre() {
+    return this._nombre;
+  }
+  get apellido() {
+    return this._apellido;
+  }
+  get edad() {
+    return this._edad;
+  }
+  set nombre(nombre) {
+    this._nombre = nombre;
+  }
+  set apellido(apellido) {
+    this._apellido = apellido;
+  }
+  set edad(edad) {
+    this._edad = edad;
+  }
 }
 
+Persona.nombre = "Juan";
+Persona.apellido = "Ortiz";
+Persona.edad = "21";
 
-Persona.nombre ="Juan";
-Persona.apellido="Ortiz"
-Persona.edad="21"
-console.log("Hola mi nombre es " + Persona.nombre + " " +  Persona.apellido + " y mi edad es " + Persona.edad + " año(s)");
+console.log(
+  "Hola mi nombre es " +
+    Persona.nombre +
+    " " +
+    Persona.apellido +
+    " y mi edad es " +
+    Persona.edad +
+    " año(s)"
+);
+
+
+class Punto {
+    constructor ( x , y ){
+      this.x = x;
+      this.y = y;
+    }
+
+    static distancia (a , b) {
+      var dx = a.x - b.x;
+      var dy = a.y - b.y;
+    //La  función Math.sqrt() retorna la raíz cuadrada de un número, que es
+      return Math.sqrt ( dx * dx + dy * dy );
+    }
+  }
+
+  var p1 = new Punto(6, 2);
+  var p2 = new Punto(4, 8);
+
+  console.log(Punto.distancia(p1, p2));
+
+  class estudiante extends Persona {
+    getnom(){
+        console.log(" Mi nuevo nombre es " + this._nombre);
+    }
+  }
+  var estudiante1 = new estudiante("Julian");
+  estudiante1.getnom();

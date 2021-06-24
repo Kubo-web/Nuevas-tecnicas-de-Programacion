083fe17e0323c01087ec33dc297867c612f758cd
Juan Pablo Ortiz <juanpablo.50cn@hotmail.com>
2021-06-24 12:08:14 -0500

    Quinta clase

---
+++ MODULO-1/PROGRAMMING/01_PROYECTS/routes/index.js
@@ -0,0 +1,19 @@
+//traemos el paquete
+
+const { Router } = require('express');
+
+//inicializamos el router
+
+const router = Router()
+
+//creacion de los endpoint
+
+
+//exportamos la rutas
+module.exports.RouterIndex = router
+
+// module = {
+//     exports: {
+//         RouterIndex: router
+//     }
+// }
